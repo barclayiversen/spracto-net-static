bbab2294e3e7f139a7cd7447d532998e56f1a4fb
Barclay <barclayiversen@gmail.com>
2024-07-12 19:18:16 -0700

    About component

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,10 +1,15 @@
+import { Archivo_Black } from "next/font/google";
+
+const play = Archivo_Black({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
 const About: React.FC = () => {
   return (
     <div className="text-center text-white py-8">
-      <h1 className="text-4xl font-bold">
-        Rootin&apos; Tootin&apos; Troubleshootin&apos;!
-      </h1>
-      <p className="mt-4 text-lg">
+      <p className={`${play.className} mt-10 text-3xl`}>
         Spracto is just a bro tryin&apos; to be a guy, man!
       </p>
     </div>
