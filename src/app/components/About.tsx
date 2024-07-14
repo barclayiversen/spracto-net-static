6c8fc65d881d89fbb89d182d3c7998960cb65f3a
Barclay <barclayiversen@gmail.com>
2024-07-14 09:36:23 -0700

    escape quotes

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -10,7 +10,7 @@ const About: React.FC = () => {
   return (
     <div className="text-center text-white py-8">
       <p className={`${play.className} mt-10 text-3xl`}>
-        "Just a bro tryin&apos; to be a guy, man!""
+        &quot;Just a bro tryin&apos; to be a guy, man!&quot;
       </p>
     </div>
   );
