b72ee3d3cee93a65e7aed05941cf86935d3a75ea
Barclay <barclayiversen@gmail.com>
2024-07-16 15:54:40 -0700

    Remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,13 @@
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Divider from "./components/Divider";
 import About from "./components/About";
-import Show from "./components/Show";
 import Releases from "./components/Releases";
 import Footer from "./components/Footer";
 import PhotoCarouselServer from "./components/PhotoLoader";
 import InstagramPosts from "./components/InstagramPosts";
 
-// Define a function to fetch the posts
+// function to fetch insta posts
 async function fetchInstagramPosts() {
   const res = await fetch(
     `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,thumbnail_url,permalink&access_token=${process.env.IG_ACCESS_TOKEN}`
@@ -26,14 +24,12 @@ const Home = async () => {
     <>
       <Header />
       <Hero />
-      {/* <About /> */}
-      {/* <Divider /> */}
-      {/* <Show /> */}
-      {/* <Divider /> */}
       <Releases />
       <InstagramPosts posts={posts} />
       <PhotoCarouselServer />
+
       <Footer />
+      <About />
     </>
   );
 };
